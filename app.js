829aa100677a1c0981480cbcc5724a5d39094ca6
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-02 11:29:47 -0400

    changed entire game to function using a nested switch

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const prompt = require('prompt-sync')();
-let gameRunning = true;
+let currentScene = 'start';
 
-while (gameRunning) {
-    const validateInput = input => {
-        if (input.toLowerCase() === 'quit game' || input.toLowerCase() === 'quit')
-            gameRunning = false;
+// Validates user input to ensure they do not want to quit
+const validateInput = input => {
+    if (input.toLowerCase() === 'quit game' || input.toLowerCase() === 'quit') {
+        gameRunning = false;
     }
-    let userInput = '';
+    else
+        return input;
+}
 
-    console.log('You are a pirate captain sailing the high seas with your crew in search of plunder and glory.');
-    console.log('At any time throughout the game, you can say "quit game" to exit.\n');
-    
-    userInput = validateInput(prompt('What is the name of your ship? '));
+// Data object for the game
+const gameData = {
+    shipName: ''
+}
 
+// Game function
+const game = () => {
+    switch (currentScene) {
+        case 'start':
+            console.log('You are a pirate captain sailing the high seas with your crew in search of plunder and glory.');
+            console.log('At any time throughout the game, you can say "quit game" to exit.\n'); 
+            console.log('What is the name of your ship?');
+            gameData.shipName = validateInput(prompt('> '));
+            break;
+        default:
+            break;
+    }
+}
+
+// let userInput = '';
+
+let gameRunning = true;
+while (gameRunning) {
+    game();
 }
