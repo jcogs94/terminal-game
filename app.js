1c1c7848ce2e38c68d123b6cfc0e23dd4e62961c
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-02 10:15:31 -0400

    added while loop for user input and beginning the game

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const prompt = require('prompt-sync')();
+let gameRunning = true;
+
+while (gameRunning) {
+    let userInput = '';
+
+    userInput = prompt('Test: ');
+
+    if (userInput.toLowerCase() === 'quit game')
+        gameRunning = false;
+}
