3cb15089b4aea476ecae84d0273c12a286192aa0
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-02 10:28:33 -0400

    input validation added as function, beginning of story added

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const prompt = require('prompt-sync')();
 let gameRunning = true;
 
 while (gameRunning) {
+    const validateInput = input => {
+        if (input.toLowerCase() === 'quit game' || input.toLowerCase() === 'quit')
+            gameRunning = false;
+    }
     let userInput = '';
 
-    userInput = prompt('Test: ');
+    console.log('You are a pirate captain sailing the high seas with your crew in search of plunder and glory.');
+    console.log('At any time throughout the game, you can say "quit game" to exit.\n');
+    
+    userInput = validateInput(prompt('What is the name of your ship? '));
 
-    if (userInput.toLowerCase() === 'quit game')
-        gameRunning = false;
 }
