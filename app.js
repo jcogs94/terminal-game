7c4d15d3be0fed3c293b08b5b916ad45effbc135
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-02 13:29:07 -0400

    updated displayStatus, incorporated userChoice for all game cases

--- app.js
+++ app.js
@@ -11,21 +11,33 @@ const validateInput = input => {
         return input;
 }
 
-const userChoice = () => {
-    let validInput = false;
+const userChoice = (options, str) => {
     let userInput = '';
     
-    while (validInput === false) {
+    while (true) {
         userInput = prompt('> ');
-        if (userInput.toUpperCase() === 'A') {
+        
+        if (userInput.toUpperCase() === 'A' && options >= 1) {
             return 'A';
         }
-        else if (userInput.toUpperCase() === 'B') {
+        else if (userInput.toUpperCase() === 'B' && options >= 2) {
             return 'B';
+        }
+        else if (userInput.toUpperCase() === 'C' && options >= 3) {
+            return 'C';
+        }
+        else if (userInput.toUpperCase() === 'D' && options >= 4) {
+            return 'D';
+        }
+        else if (userInput.toUpperCase() === 'E' && options >= 5) {
+            return 'E';
         }
         else if (userInput.toLowerCase() === 'quit game' || userInput.toLowerCase() === 'quit') {
             currentScene = 'end';
-            break;
+            return 'end';
+        }
+        else if (str === true) {
+            return userInput;
         }
         else {
             console.log('Invalid input, try again.');
@@ -39,11 +51,20 @@ const gameData = {
     shipLocation: 'Off the coast of port'
 }
 
+// Displays important ship data
 const displayStatus = () => {
     console.clear()
+    
+    let lineSeparator = '';
+    for (let i = 0; i < 50; i++) {
+        lineSeparator += '-';
+    }
+    
+    console.log(lineSeparator);
     console.log(gameData.shipName);
     console.log(`Location: ${gameData.shipLocation}`);
-    console.log('\n');
+    console.log(lineSeparator);
+    console.log();
 }
 
 // Game function
@@ -54,35 +75,22 @@ const game = () => {
             console.log('You are a pirate captain sailing the high seas with your crew in search of plunder and glory.');
             console.log('At any time throughout the game, you can say "quit game" to exit.\n'); 
             console.log('What is the name of your ship?');
-            gameData.shipName = validateInput(prompt('> '));
-            currentScene = 'start';
-            break;
+            
+            let choiceInit = userChoice(0, true);
+            if (choiceInit === 'end')
+                break;
+            else {
+                gameData.shipName = choiceInit;
+                currentScene = 'start';
+                break;
+            }
         case 'start':
             displayStatus();
             console.log('You are currently anchored off the coast of an island port. What do you instruct your crew to do?');
             console.log('(A) Go out to sea.');
             console.log('(B) Dock at the port for supplies.\n');
             
-            let validInput = false;
-            let userInput = '';
-            while (validInput === false) {
-                userInput = prompt('> ');
-                if (userInput.toUpperCase() === 'A') {
-                    validInput = true;
-                    currentScene = 'atSea';
-                }
-                else if (userInput.toUpperCase() === 'B') {
-                    validInput = true;
-                    currentScene = 'atPort';
-                }
-                else if (userInput.toLowerCase() === 'quit game' || userInput.toLowerCase() === 'quit') {
-                    currentScene = 'end';
-                    break;
-                }
-                else {
-                    console.log('Invalid input, try again.');
-                }
-            }
+            let choiceStart = userChoice(2);
 
             break;
         case 'end':
